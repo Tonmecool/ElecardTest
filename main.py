3e999753df7e7679528b3197b49327d354a49b67
Владимир Погребной <qpwoei.86@mail.ru>
2023-09-25 10:58:00 +0300

    code readability

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ logging.basicConfig(level=logging.DEBUG, filename="logging.log", filemode="w",
 
 
 # Авторизация + получение токена
-async def Authorization(session):
+async def authorization(session):
     try:
         async with session.post(login_check, json=user_json) as resp:
             logging.info(f'Authorization Response status: {resp.status}')
@@ -41,25 +41,27 @@ async def Authorization(session):
             temp = await resp.json()
             token = temp['response']['token']
             logging.info(f'Authorization access pass, token: {token}')
-            return token   
+            return token
     except Exception as error:
         logging.critical(f'Authorization error: {error}')
 
+
 # Создание группы
-async def CreateGroup(session, token):
+async def create_group(session, token):
     try:
         async with session.post(group, json=group_add_json, headers={'Authorization': f'Bearer {token}'}) as resp:
             logging.info(f'Group add Response status: {resp.status}')
             assert resp.status == good_code, f"Group add fail, Response: {resp.text()}"
             temp = await resp.json()
             group_id = temp['response']['id']
-            logging.info(f'Group add pass, Group id: {group_id}') 
+            logging.info(f'Group add pass, Group id: {group_id}')
             return group_id
     except Exception as error:
         logging.error(f'Group add error: {error}')
 
+
 # Удаление группы
-async def DeleteGroup(session, group_id, token):
+async def delete_group(session, group_id, token):
     try:
         async with session.delete(group, params={'id': group_id}, headers={'Authorization': f'Bearer {token}'}) as resp:
             logging.info(f'Group delete Response status: {resp.status}')
@@ -68,8 +70,9 @@ async def DeleteGroup(session, group_id, token):
     except Exception as error:
         logging.error(f'Group delete error: {error}')
 
+
 # Создание плеера
-async def CreatePlayer(session, token):
+async def create_player(session, token):
     try:
         async with session.post(player, json=player_add_json, headers={'Authorization': f'Bearer {token}'}) as resp:
             logging.info(f'Player add Response status: {resp.status}')
@@ -81,37 +84,42 @@ async def CreatePlayer(session, token):
     except Exception as error:
         logging.error(f'Player add error: {error}')
 
-# Добавление плеера в группу 
-async def PlayerToGroup(session, player_id, group_id, token):
+
+# Добавление плеера в группу
+async def player_to_group(session, player_id, group_id, token):
     try:
-        async with session.put(player, params={'id': player_id}, json=player_add_json, headers={'Authorization': f'Bearer {token}'}) as resp:
+        async with session.put(player, params={'id': player_id}, json=player_add_json,
+                               headers={'Authorization': f'Bearer {token}'}) as resp:
             logging.info(f'Player add to Group Response status: {resp.status}')
             assert resp.status == good_code, f"Player add to Group fail, Response: {resp.text()}"
             logging.info(f'Player add to Group pass, Player id: {player_id}, Group id: {group_id}')
     except Exception as error:
         logging.error(f'Player add to Group error: {error}')
 
+
 # Удаление плеера
-async def DeletePlayer(session, player_id, token):
+async def delete_player(session, player_id, token):
     try:
-        async with session.delete(player, params={'id': player_id}, headers={'Authorization': f'Bearer {token}'}) as resp:
+        async with session.delete(player, params={'id': player_id},
+                                  headers={'Authorization': f'Bearer {token}'}) as resp:
             logging.info(f'Player delete Response status: {resp.status}')
             assert resp.status == good_code, f"Player delete fail, Response: {resp.text()}"
             logging.info('Player delete pass')
     except Exception as error:
         logging.error(f'Player delete error: {error}')
 
+
 # Основные действия скрипта
 async def main():
     async with aiohttp.ClientSession(url) as session:
         logging.info('Authorization Request')
-        token = await Authorization(session)
+        token = await authorization(session)
 
         logging.info('Group add Request')
-        group_id = await CreateGroup(session, token)
+        group_id = await create_group(session, token)
 
         logging.info('Player add Request')
-        player_id = await CreatePlayer(session, token)
+        player_id = await create_player(session, token)
 
         # Изменение ID группы
         try:
@@ -120,12 +128,13 @@ async def main():
             logging.error(f'Group id error: {error}')
 
         logging.info('Player add to Group Request')
-        await PlayerToGroup(session, player_id, group_id, token)
+        await player_to_group(session, player_id, group_id, token)
 
         logging.info('Player delete Request')
-        await DeletePlayer(session, player_id, token)
+        await delete_player(session, player_id, token)
 
         logging.info('Group delete Request')
-        await DeleteGroup(session, group_id, token)
+        await delete_group(session, group_id, token)
+
 
 asyncio.run(main())
